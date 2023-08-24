2ecbc8fd6f410500faa6f2d6aa39dce5c5bded96
lotusloli <nymphaeal@qq.com>
2023-08-24 17:47:36 +0800

    1. change vite base;

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/jing-que/',
   plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
